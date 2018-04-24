5d3ee3d1d338e6e9cb976a25906fff4ee559bb91
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-23 22:47:28 -0500

    added connect, created event handlers for form

--- src/agent.js
+++ src/agent.js
@@ -10,9 +10,11 @@ const API_ROOT = 'http://conduit.productionready.io/api'
 const responseBody = res => res.body
 
 const requests = {
-	get: url => superagent.get(`${API_ROOT}${url}`).then(responseBody)
+	get: url => superagent.get(`${API_ROOT}${url}`).then(responseBody),
+	post: (url, body) => superagent.post(`${API_ROOT}${url}`, body).then(responseBody)
 }
 
 const Articles = { all: page => requests.get('/articles') }
+const Auth = { login: (email, password) => {requests.post('/users/login', { user: { email, password } })}}
 
-export default { Articles }
+export default { Articles, Auth }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import agent from '../agent'
+import ListErrors from './ListErrors'
+
+const mapStateToProps = state => ({ ...state.auth })
+const mapDispatchToProps = dispatch => ({
+	onChangeEmail:    val => dispatch({ type: 'UPDATE_FIELD_AUTH', key: 'email', val }),
+	onChangePassword: val => dispatch({ type: 'UPDATE_FIELD_AUTH', key: 'password', val }),
+	onSubmitForm:     (email, password) => dispatch({ type: 'LOGIN', payload: agent.Auth.login(email, password) })
+})
 
 class Login extends Component {
+	constructor() {
+		super()
+		this.changeEmail = e => this.props.onChangeEmail(e.target.value)
+		this.changePassword = e => this.props.onChangePassword(e.target.value)
+		this.submitForm = (email, password) => e => {
+			e.preventDefault()
+			this.props.onSubmitForm(email, password)
+		}
+
+	}
+
 	render() {
+		const { email, password } = this.props
+
 		return (
 			<div className="auth-page">
 				<div className="container page">
@@ -12,16 +34,29 @@ class Login extends Component {
 								Sign In
 							</h1>
 							<p className="text-xs-center">
-								<a href="">
-									Need account?
-								</a>
+								<a href=""> Need account? </a>
 							</p>
-							<form action="">
+							<ListErrors errors={ this.props.errors } />
+							<form onSubmit={ this.submitForm(email, password) }>
 								<fieldset className="form-group">
-									<input className="form-control form-control-lg" type="email" placeholder="Email"/>
-									<input type="password" className="form-control form-control-lg" placeholder="Password"/>
+									<input
+										className="form-control form-control-lg"
+										type="email" placeholder="Email"
+										value={ email }
+										onChange={ this.changeEmail }/>
+									<input
+										type="password"
+										className="form-control form-control-lg"
+										placeholder="Password"
+										value={ password }
+										onChange={ this.changePassword }/>
 								</fieldset>
-								<button className="btn btn-lg btn-primary pull-xs-right" type="submit">Sign In</button>
+								<button
+									className="btn btn-lg btn-primary pull-xs-right"
+									type="submit"
+								disabled={ this.props.inProgress }>
+									Sign In
+								</button>
 							</form>
 						</div>
 					</div>
@@ -31,4 +66,4 @@ class Login extends Component {
 	}
 }
 
-export default connect(() => ({}), () => ({}))(Login)
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
