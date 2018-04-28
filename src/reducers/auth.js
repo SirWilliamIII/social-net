a8251d9b6d48bdf3b62b9fc5d1cfac312cc04aea
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-27 19:25:51 -0500

    moved/created reducers

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,3 +1,20 @@
+/* eslint-disable */
 export default (state={}, action) => {
+	switch (action.type) {
+		case 'LOGIN':
+			return {
+				...state,
+				inProgress: false,
+				errors: action.error ? action.payload.errors : null
+			}
+		case 'ASYNC_START':
+			if(action.subtype === 'LOGIN' || action.sub === 'REGISTER') {
+				return { ...state, inProgress: true }
+			}
+			break
+		case 'UPDATE_FIELD_AUTH':
+			return { ...state, [action.key]: action.value }
+
+	}
 	return state
 }
--- src/reducers/home.js
+++ src/reducers/home.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default (state = {}, action) => {
 	switch (action.type) {
 		case 'HOME_PAGE_LOADED':
