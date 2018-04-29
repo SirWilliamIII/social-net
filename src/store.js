333db781a98c4d7b941e87fc383060f21ef3051a
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-29 15:55:51 -0500

    add reducers to main reducers

--- src/store.js
+++ src/store.js
@@ -7,14 +7,21 @@ import common from './reducers/common'
 import home from './reducers/home'
 import settings from './reducers/settings'
 import article from './reducers/article'
+import articleList from './reducers/articleList'
+import profile from './reducers/profile'
+import editor from './reducers/editor'
 
 // import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
+	article,
+	articleList,
 	auth,
 	common,
 	home,
-	settings
+	settings,
+	profile,
+	editor
 })
 
 // const devTools = composeWithDevTools()
