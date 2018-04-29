f7ee5ab469c3bedd0548df43c3e28715386c5091
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-29 17:06:25 -0500

    added ion icon cdn

--- src/components/Article/ArticleActions.js
+++ src/components/Article/ArticleActions.js
@@ -4,7 +4,11 @@ import agent from '../../agent'
 import { connect } from 'react-redux'
 
 const mapDispatchToProps = dispatch => ({
-	onClickDelete: payload => dispatch({ type: 'DELETE_ARTICLE', payload })
+	onClickDelete: payload =>
+		dispatch({
+			type: 'DELETE_ARTICLE',
+			payload
+		})
 })
 
 const ArticleActions = props => {
@@ -15,11 +19,10 @@ const ArticleActions = props => {
 	if(props.canModify) {
 		return (
 			<span>
-
-        <Link
-	        to={ `/editor/${article.slug}` }
-	        className="btn btn-outline-secondary btn-sm">
-          <i className="ion-edit">''</i> Edit Article
+		        <Link
+	                to={ `/editor/${article.slug}` }
+	                className="btn btn-outline-secondary btn-sm">
+                <i className="ion-edit">Edit Article</i>
         </Link>
 
         <button className="btn btn-outline-danger btn-sm" onClick={ del }>
